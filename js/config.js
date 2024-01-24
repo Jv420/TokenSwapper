ca911cab19c0cbd1586bb2acae0f87cc434b7d74
Jv420 <136645216+Jv420@users.noreply.github.com>
2024-01-24 20:22:20 +0100

    config.js

--- js/config.js
+++ js/config.js
@@ -12,4 +12,4 @@ var ChainName= "Binance"; //find this on  https://cryptodapprun.github.io/Blockc
 
 var BlockchainScanURL="https://bscscan.com/tx/";  //blockchain RPC url 
 var contractAdress="0xD32F1544c36D0942De599Bb1c8b36F940e2f9853"  //deploy contract/SwapToken.sol,then you will get the address
-var TokenAddress  ="0x0F6FC13d26EeA45d3cdA81bCBcb0b7bE711BA3fE"  //your token address
+var TokenAddress  ="0x655925A3e2D561d60a2825f61f570A8Fb8C8BfB2"  //your token address
